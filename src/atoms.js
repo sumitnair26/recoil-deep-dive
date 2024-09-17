fa550600b1f1cf71377d27c71c6ce3bf023e532e
sumitnair26 <sumit.nair26@gmail.com>
2024-09-17 14:14:10 +0530

    added atom.js

---
+++ src/atoms.js
@@ -0,0 +1,22 @@
+import { atom } from "recoil";
+
+export const networkAtom = atom({
+    key: "networkAtom",
+    default: 104
+})
+
+export const jobsAtom = atom({
+    key:"jobAtom",
+    default: 10
+})
+
+export const messagingAtom = atom({
+    key:"messagingAtom",
+    default: 0
+})
+
+export const notificationAtom = atom({
+    key:"notificationAtom",
+    default: 102
+})
+

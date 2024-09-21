6436320feef46da7790c66a56b4cc6d08c96205e
sumitnair26 <sumit.nair26@gmail.com>
2024-09-21 20:34:29 +0530

    recoil state loadable

--- src/components/TodoSelectorFamily.jsx
+++ src/components/TodoSelectorFamily.jsx
@@ -4,9 +4,14 @@ import { todosSecAtomFamily } from "../atoms"
 export const TodoSelectorFamily = ({id}) => {
    // const getCurrentTodo = useRecoilValue(todoSelecAtomFamily(id))
     const [getCurrentTodo, setTodo] = useRecoilStateLoadable(todosSecAtomFamily(id));
-
-    return <>
-        <h1>{getCurrentTodo.title}</h1>
-        <h2>{getCurrentTodo.description}</h2>
-    </>
+    if (getCurrentTodo.state === "loading") {
+        return <div>
+            Loading..... 
+        </div>
+    } else if(getCurrentTodo.state === "hasValue") {
+        return <>
+            <h1>{getCurrentTodo.contents.title}</h1>
+            <h2>{getCurrentTodo.contents.description}</h2>
+        </>
+    }
 }
